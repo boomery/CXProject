88479ef14d361d583ff860e8912a39795f3b1c2b
boomery <zhangchaoxin@zhangchaoxindeMacBook-Pro.local>
2017-03-22 15:42:37 +0800

    Initial Commit

---
+++ CXProject/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CXProject
+//
+//  Created by zhangchaoxin on 2017/3/22.
+//  Copyright © 2017年 zhangchaoxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
