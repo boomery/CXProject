f8265e7fd962569c2cb7e402a2219e1219a0dd07
zhangchaoxin <zhangchaoxin@zhangchaoxindeMacBook-Pro.local>
2017-05-11 13:39:51 +0800

    添加风险评估页面

---
+++ CXProject/录入/Controllers/RiskViewController.h
@@ -0,0 +1,13 @@
+//
+//  RiskViewController.h
+//  CXProject
+//
+//  Created by zhangchaoxin on 2017/5/11.
+//  Copyright © 2017年 zhangchaoxin. All rights reserved.
+//
+
+#import "ViewPagerController.h"
+
+@interface RiskViewController : ViewPagerController
+
+@end
