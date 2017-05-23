5c41a4ff62f53f5dcc7e15a2cfd1efad16f6107b
zhangchaoxin <zhangchaoxin@zhangchaoxindeMacBook-Pro.local>
2017-05-23 14:31:55 +0800

    使用系统级音频播播放代码

--- CXProject/Utils/音频播放/SoundPlayer.m
+++ CXProject/Utils/音频播放/SoundPlayer.m
@@ -30,11 +30,20 @@ static SoundPlayer *sharedPlayer = nil;
     NSString *string = @"0123456789";
     if ([string containsString:name])
     {
-        NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:name ofType:@"WAV"]];
-        sharedPlayer.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
-        sharedPlayer.player.numberOfLoops = 0;
-        sharedPlayer.player.volume = 0.8;
-        [sharedPlayer.player play];
+//        NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:name ofType:@"WAV"]];
+//        sharedPlayer.player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:NULL];
+//        sharedPlayer.player.numberOfLoops = 0;
+//        sharedPlayer.player.volume = 0.8;
+//        [sharedPlayer.player play];
+        SystemSoundID soundID;
+        //NSBundle来返回音频文件路径
+        NSString *soundFile = [[NSBundle mainBundle] pathForResource:name ofType:@"WAV"];
+        //建立SystemSoundID对象，但是这里要传地址(加&符号)。 第一个参数需要一个CFURLRef类型的url参数，要新建一个NSString来做桥接转换(bridge)，而这个NSString的值，就是上面的音频文件路径
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef)[NSURL fileURLWithPath:soundFile], &soundID);
+        //播放提示音 带震动
+        AudioServicesPlayAlertSound(soundID);
+        //播放系统声音
+        //    AudioServicesPlaySystemSound(soundID);
     }
 }
 
